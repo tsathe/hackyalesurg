451c12d3752fa37eba2b6ec60cdff9b3db493281
tsathe <tejas.sathe@gmail.com>
2018-05-07 03:18:25 -0400

    js

---
+++ js/htmlform.js
@@ -0,0 +1,14 @@
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbw2fy_RhXXbW0IpPuDH8ppiNx1ji06AzIJ-ooSQrxCsEHsued0/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
