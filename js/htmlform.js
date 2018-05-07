22dfdc93f48240ed7b1cf6cc097e0e78f5c72cc9
tsathe <tejas.sathe@gmail.com>
2018-05-07 11:19:59 -0400

    update

--- js/htmlform.js
+++ js/htmlform.js
@@ -1,14 +1,9 @@
-var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbw2fy_RhXXbW0IpPuDH8ppiNx1ji06AzIJ-ooSQrxCsEHsued0/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw2fy_RhXXbW0IpPuDH8ppiNx1ji06AzIJ-ooSQrxCsEHsued0/exec'
+const form = document.forms['submit-to-google-sheet']
 
-$('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-  );
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
 })
