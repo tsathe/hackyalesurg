d50ca9a301186b2becbab48c9357233df2d11fad
tsathe <tejas.sathe@gmail.com>
2018-05-17 01:40:05 -0400

    hello

--- js/htmlform.js
+++ js/htmlform.js
@@ -1,9 +1,14 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw2fy_RhXXbW0IpPuDH8ppiNx1ji06AzIJ-ooSQrxCsEHsued0/exec'
-const form = document.forms['submit-to-google-sheet']
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbw2fy_RhXXbW0IpPuDH8ppiNx1ji06AzIJ-ooSQrxCsEHsued0/exec'
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
 })
